3a43518d500fddbb28744f1799072fb6b28c67a4
campbell <campbell.com>
2022-09-01 12:12:08 -0500

    bunch of changes

---
+++ print_customers.py
@@ -0,0 +1,19 @@
+import csv
+
+infile = open('customers.csv','r')
+
+csvfile = csv.reader(infile,delimiter=',')
+
+next(csvfile)
+
+for record in csvfile:
+    
+    print ('student ID:',record[0])
+    print('fname:',record[1])
+    print('lname:',record[2])
+    print('city:',record[3])
+    print('country:',record[4])
+    print('phone:',record[5])
+
+
+    input()
